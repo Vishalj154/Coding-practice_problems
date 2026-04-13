4893584bda1d5e02fcb249bb07771e35d54ee953
Vishal <vishaljankar2534@gmail.com>
2026-04-13 23:03:17 +0530

    logic improve

--- 75_sort_colors.py
+++ 75_sort_colors.py
@@ -1,23 +1,13 @@
 class Solution:
-    def sortcolors(self, nums,low,high) -> None:
+    def sortcolors(self, nums) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low=0
-        high=len(nums)-1
-        def partition(self,nums,low,high):
-            pi=len(nums)-1
-            i=low-1
-            for j in range (low,high):
-                if(nums[j]<nums[pi]):
-                    i+=1
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[j] < nums[i]):
                     nums[i],nums[j]=nums[j],nums[i]
-            nums[i+1],nums[pi]=nums[pi],nums[i+1]
-            return i+1
-        if(low < high):
-            pi=partition(self,nums,low,high)
-            self.sortcolors(nums,low,pi-1)
-            self.sortcolors(nums,pi+1,high)
+            
         
         print(nums)
             
@@ -26,4 +16,4 @@ class Solution:
             
         
 const=Solution()  
-const.sortcolors([2,0,2,1,1,0],0,len([2,0,2,1,1,0])-1)
+const.sortcolors([2,0,2,1,1,0])
