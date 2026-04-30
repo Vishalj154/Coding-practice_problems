ef5c47e615a7e7e725c69ae2a70f07df72e8e4a2
Vishal <vishaljankar2534@gmail.com>
2026-04-30 20:51:51 +0530

    2nd try + ai

--- 21_merge_two_sorted_list.py
+++ 21_merge_two_sorted_list.py
@@ -1,22 +1,28 @@
-class Solution:
-    def__init___(list1,list2):
-        list3=list1+list2
-        print(list3)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    def merge_sort(list3)
-        mid=len(list3)/2
-        for i in range(0:mid):
-            n1[i]=list[1]
-        j=0
-        for i in range(mid,len(list3)):
-            n2[j]=list3[i]
-        i=0,s=0
-        for k in range(0,len(list3)):
-            if(n1[i]<n2[s]):
-                list3[k]=n[i]
-                i++
+
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
             else:
-                list3[k]=n2[s]
-                s++
-    return list3
-        
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        # attach remaining nodes AFTER loop
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
