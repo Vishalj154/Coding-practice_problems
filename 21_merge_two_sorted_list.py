44f14f9df867a9692d0cc2ae4a1bff6e402b0295
Vishal <vishaljankar2534@gmail.com>
2026-04-30 20:33:19 +0530

    1st try

---
+++ 21_merge_two_sorted_list.py
@@ -0,0 +1,22 @@
+class Solution:
+    def__init___(list1,list2):
+        list3=list1+list2
+        print(list3)
+
+    def merge_sort(list3)
+        mid=len(list3)/2
+        for i in range(0:mid):
+            n1[i]=list[1]
+        j=0
+        for i in range(mid,len(list3)):
+            n2[j]=list3[i]
+        i=0,s=0
+        for k in range(0,len(list3)):
+            if(n1[i]<n2[s]):
+                list3[k]=n[i]
+                i++
+            else:
+                list3[k]=n2[s]
+                s++
+    return list3
+        
