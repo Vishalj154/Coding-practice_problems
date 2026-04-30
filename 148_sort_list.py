150b113ab4f846e007de39d61ca7ee20d32089f9
Vishal <vishaljankar2534@gmail.com>
2026-04-30 21:23:14 +0530

    final logic

--- 148_sort_list.py
+++ 148_sort_list.py
@@ -29,4 +29,9 @@ class Solution:
                 tail.next=l2
                 l2=l2.next
             tail=tail.next
+
+            if l1:
+                tail.next = l1
+            else:
+                tail.next = l2
         return dummy.next
