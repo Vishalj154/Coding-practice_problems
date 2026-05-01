556ea685bb61f8efaa1382dbc50a34d580505408
Vishal <vishaljankar2534@gmail.com>
2026-05-01 15:11:36 +0530

    most final done question no.04

--- 704_binary_search.py
+++ 704_binary_search.py
@@ -2,7 +2,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low=0
         high=len(nums)-1
-        while low < high:
+        while low <= high:
             mid=(low+high)//2
 
             if target==nums[mid]:
