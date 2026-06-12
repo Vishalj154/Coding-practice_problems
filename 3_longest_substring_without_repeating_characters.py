47bf3f27b1d6e0e9496c1c0be6812523b87c0858
Vishal <vishaljankar2534@gmail.com>
2026-06-12 21:39:38 +0530

    sliding window approach for 3

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -1,21 +1,21 @@
+# sliding window approach lift right ppointer
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        ans=0
-        
-        for i in range(len(s)):
-            seen=set()
-            for j in range(i,len(s)):
-                if s[j] in seen:
-                    break
-                
-                seen.add(s[j])
-                ans = max(ans, len(seen))
-        
-        
+        seen = set()
+        left = 0
+        ans = 0
 
+        for right in range(len(s)):
+
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+
+            seen.add(s[right])
+
+            ans = max(ans, right - left + 1)
 
         return ans
-            
 c=Solution()
 print(c.lengthOfLongestSubstring("pwwkew"))
 
