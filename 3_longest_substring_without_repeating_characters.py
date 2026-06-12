59f6409f62a1446c851743b449781769924c5ea3
Vishal <vishaljankar2534@gmail.com>
2026-06-12 21:08:47 +0530

    logic started for 3

---
+++ 3_longest_substring_without_repeating_characters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        long=0
+        count=0
+        for i in range(len(s)):
+            print(s[i])
+            
+c=Solution()
+c.lengthOfLongestSubstring("abcabcbb")
+
+        
