0e4604b1329d9843001f586e54b43e894b67b1f8
Vishalj154 <vishaljankar2534@gmail.com>
2026-07-11 20:49:12 +0530

    final solution using binary search for 4

--- 4_median_of_TwoSortedArrays.py
+++ 4_median_of_TwoSortedArrays.py
@@ -1,6 +1,11 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums3=nums1 + nums2
+        nums3=sorted(nums1 + nums2)
+        left=0
+        right=len(nums3)-1
         if len(nums3)%2!=0:
-            
-        return (max(nums3)+min(nums3))/2
+            return float(nums3[(left+right)//2])
+        else:
+            mid1=nums3[(left+right)//2] 
+            mid2=nums3[(left+right)//2+1] 
+            return (mid1+mid2)/2
