a5853a6d085562f4cfcedf78a62a898a68f0f4bc
Vishalj154 <vishaljankar2534@gmail.com>
2026-07-14 17:36:02 +0530

    solution for 38

--- 38_CombinatinSum.py
+++ 38_CombinatinSum.py
@@ -2,3 +2,15 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result=[]
        
+        def backtrack(index, remaining, combination):
+            if remaining==0:
+                result.append(combination[:])
+                return
+            if remaining<0:
+                return
+            for i in range(index,len(candidates)):
+                combination.append(candidates[i])
+                backtrack(i,remaining-candidates[i],combination)
+                combination.pop()
+        backtrack(0,target,[])
+        return result
