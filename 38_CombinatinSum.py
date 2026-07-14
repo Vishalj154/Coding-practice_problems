7040012e36523d7d35f50fc7a2d0732829adcdc7
Vishalj154 <vishaljankar2534@gmail.com>
2026-07-14 17:24:06 +0530

    for problem 78 logic added

--- 38_CombinatinSum.py
+++ 38_CombinatinSum.py
@@ -1,12 +1,4 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        s=[]
-        for num in candiadtes:
-            if target%num==0:
-                q=target//num
-                s.append([num]*q)
-        i=0
-        while i<len(nums):
-            q=target-candidates[i]
-            if q in candidates:
-                s.join([candidates[i],q])
+        result=[]
+       
