4c1a7327998c6542a2c6e26e3d0e44d6c9fed9e4
Vishalj154 <vishaljankar2534@gmail.com>
2026-07-14 17:25:52 +0530

    final logic for 78

--- 78_subsets.py
+++ 78_subsets.py
@@ -1,12 +1,12 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums) :
         result=[]
         def backtrack(i,subset):
             if i==len(nums):
                 result.append(subset[:])
                 return
 
-            subset.append([nums[i]])
+            subset.append(nums[i])
             backtrack(i+1,subset)
             subset.pop()
             backtrack(i + 1, subset)
