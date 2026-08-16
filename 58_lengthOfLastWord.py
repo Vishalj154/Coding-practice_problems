96145b8dc1b2f24ca7b7d315b541a8a2fb3482e5
Vishalj154 <vishaljankar2534@gmail.com>
2026-08-16 12:46:02 +0530

    done 58

--- 58_lengthOfLastWord.py
+++ 58_lengthOfLastWord.py
@@ -1,4 +1,7 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s=list(map(split(",")))
+    def lengthOfLastWord(self, s) -> int:
+        s=s.split( )
         return len(s[-1])
+
+c=Solution()
+print(c.lengthOfLastWord("Hello World"))
